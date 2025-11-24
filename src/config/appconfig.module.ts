76bf89f006d600d26aa5604381058082cc338bdb
matGitMen <matemgm6@gmail.com>
2025-11-24 15:47:44 -0500

    Delete src directory

--- src/config/appconfig.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DbConfigService } from './services/dbconfig.service';
-import { ConfigService } from '@nestjs/config';
-
-@Module({
-  providers: [DbConfigService, ConfigService],
-  exports: [DbConfigService],
-})
-export class AppConfigModule {}
